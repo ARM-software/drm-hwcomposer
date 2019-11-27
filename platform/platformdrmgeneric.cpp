ARM-software/drm-hwcomposer

commit f63726cabf3f1c940230e7b96fd514fca6dfb906

drm_hwcomposer: avoid using signed errno on uint32 type
DrmGenericImporter::ConvertHalFormatToDrm() should not return negative values.

 - Use DRM_FORMAT_INVALID instead of -EINVAL
 - Check DrmGenericImporter::ConvertHalFormatToDrm() result value in
   DrmGenericImporter::ImportBuffer()

Signed-off-by: Roman Stratiienko <roman.stratiienko@globallogic.com>

platform/platformdrmgeneric.cpp
@@ -89,7 +89,7 @@ uint32_t DrmGenericImporter::ConvertHalFormatToDrm(uint32_t hal_format) {
       return DRM_FORMAT_YVU420;
     default:
       ALOGE("Cannot convert hal format to drm format %u", hal_format);
-      return -EINVAL;
+      return DRM_FORMAT_INVALID;
   }
 }
 
@@ -128,6 +128,8 @@ int DrmGenericImporter::ImportBuffer(buffer_handle_t handle, hwc_drm_bo_t *bo) {
   bo->height = gr_handle->height;
   bo->hal_format = gr_handle->format;
   bo->format = ConvertHalFormatToDrm(gr_handle->format);
+  if (bo->format == DRM_FORMAT_INVALID)
+    return -EINVAL;
   bo->usage = gr_handle->usage;
   bo->pixel_stride = (gr_handle->stride * 8) /
                      DrmFormatToBitsPerPixel(bo->format);

platform/platformhisi.cpp
@@ -121,9 +121,9 @@ int HisiImporter::ImportBuffer(buffer_handle_t handle, hwc_drm_bo_t *bo) {
     return ret;
   }
 
-  int32_t fmt = ConvertHalFormatToDrm(hnd->req_format);
-  if (fmt < 0)
-    return fmt;
+  uint32_t fmt = ConvertHalFormatToDrm(hnd->req_format);
+  if (fmt == DRM_FORMAT_INVALID)
+    return -EINVAL;
 
   is_rgb = IsDrmFormatRgb(fmt);
   modifiers[0] = ConvertGrallocFormatToDrmModifiers(hnd->internal_format,

platform/platformmeson.cpp
@@ -98,9 +98,9 @@ int MesonImporter::ImportBuffer(buffer_handle_t handle, hwc_drm_bo_t *bo) {
     return ret;
   }
 
-  int32_t fmt = ConvertHalFormatToDrm(hnd->req_format);
-  if (fmt < 0)
-    return fmt;
+  uint32_t fmt = ConvertHalFormatToDrm(hnd->req_format);
+  if (fmt == DRM_FORMAT_INVALID)
+    return -EINVAL;
 
   modifiers[0] = ConvertGrallocFormatToDrmModifiers(hnd->internal_format);
 

commit 6f5e450d50952e5354bafabae7b4001dbf4c35eb

drm_hwcomposer: Fix missing definition build error on P and earlier
DRM_FORMAT_INVALID was introduced after libdrm-2.4.94, and does not exists in
Android-P and earlier.

Fixes: f63726cabf3f ("drm_hwcomposer: avoid using signed errno on uint32 type")
Signed-off-by: Roman Stratiienko <roman.stratiienko@globallogic.com>

platform/platformdrmgeneric.cpp
@@ -20,7 +20,6 @@
 #include "drmdevice.h"
 #include "platform.h"
 
-#include <drm/drm_fourcc.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 

platform/platformdrmgeneric.h
@@ -22,6 +22,12 @@
 
 #include <hardware/gralloc.h>
 
+#include <drm/drm_fourcc.h>
+
+#ifndef DRM_FORMAT_INVALID
+#define DRM_FORMAT_INVALID 0
+#endif
+
 namespace android {
 
 class DrmGenericImporter : public Importer {

platform/platformhisi.cpp
@@ -20,7 +20,6 @@
 #include "drmdevice.h"
 #include "platform.h"
 
-#include <drm/drm_fourcc.h>
 #include <stdatomic.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>

platform/platformimagination.cpp
@@ -1,7 +1,6 @@
 #define LOG_TAG "hwc-platform-imagination"
 
 #include "platformimagination.h"
-#include <drm/drm_fourcc.h>
 #include <log/log.h>
 #include <xf86drm.h>
 

platform/platformmeson.cpp
@@ -20,7 +20,6 @@
 #include "drmdevice.h"
 #include "platform.h"
 
-#include <drm/drm_fourcc.h>
 #include <stdatomic.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>

platform/platformminigbm.cpp
@@ -20,7 +20,6 @@
 #include "drmdevice.h"
 #include "platform.h"
 
-#include <drm/drm_fourcc.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 