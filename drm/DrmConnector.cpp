ARM-software/drm-hwcomposer

commit adc5d8229b9ce25f1bf0e4d02bb838eae62b89fc

drm_hwcomposer: Fix EDID fetch from DRM
EDID fetch procedure consist of two steps:
1) get appropriate blob_id
2) fetch EDID blob using blob_id
Those steps should be done sequentially, in
other case drivers can update EDID and blob_id
won't be actual.

Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

drm/DrmConnector.cpp
@@ -58,10 +58,7 @@ int DrmConnector::Init() {
     ALOGE("Could not get CRTC_ID property\n");
     return ret;
   }
-  ret = drm_->GetConnectorProperty(*this, "EDID", &edid_property_);
-  if (ret) {
-    ALOGW("Could not get EDID property\n");
-  }
+  ret = UpdateEdidProperty();
   if (writeback()) {
     ret = drm_->GetConnectorProperty(*this, "WRITEBACK_PIXEL_FORMATS",
                                      &writeback_pixel_formats_);
@@ -82,6 +79,30 @@ int DrmConnector::Init() {
       return ret;
     }
   }
+  return 0;
+}
+
+int DrmConnector::UpdateEdidProperty() {
+  int ret = drm_->GetConnectorProperty(*this, "EDID", &edid_property_);
+  if (ret) {
+    ALOGW("Could not get EDID property\n");
+  }
+  return ret;
+}
+
+int DrmConnector::GetEdidBlob(drmModePropertyBlobPtr &blob) {
+  uint64_t blob_id;
+  int ret = UpdateEdidProperty();
+  if (ret) {
+    return ret;
+  }
+
+  std::tie(ret, blob_id) = edid_property().value();
+  if (ret) {
+    return ret;
+  }
+
+  blob = drmModeGetPropertyBlob(drm_->fd(), blob_id);
   return 0;
 }
 

DrmHwcTwo.cpp
@@ -915,16 +915,11 @@ HWC2::Error DrmHwcTwo::HwcDisplay::GetDisplayIdentificationData(
   supported(__func__);
 
   drmModePropertyBlobPtr blob;
-  int ret;
-  uint64_t blob_id;
 
-  std::tie(ret, blob_id) = connector_->edid_property().value();
-  if (ret) {
+  if (connector_->GetEdidBlob(blob)) {
     ALOGE("Failed to get edid property value.");
     return HWC2::Error::Unsupported;
   }
-
-  blob = drmModeGetPropertyBlob(drm_->fd(), blob_id);
 
   if (outData) {
     *outDataSize = std::min(*outDataSize, blob->length);

drm/DrmConnector.h
@@ -40,6 +40,8 @@ class DrmConnector {
   DrmConnector &operator=(const DrmProperty &) = delete;
 
   int Init();
+  int UpdateEdidProperty();
+  int GetEdidBlob(drmModePropertyBlobPtr &blob);
 
   uint32_t id() const;
 

commit 25ddbc44acfbe6d53439deb68e0f92b5809b5738

drm_hwcomposer: Return error from GetEdidBlob if blob is null
drmModeGetPropertyBlob can return NULL blobs resulting in a segfault in
GetDisplayIdentificationData which expects blob to be set to a valid
pointer when GetEdidBlob returns 0.

Fixes: adc5d8229b9ce25f1bf0e4d02bb838eae62b89fc

Signed-off-by: Marijn Suijten <marijns95@gmail.com>

drm/DrmConnector.cpp
@@ -103,7 +103,7 @@ int DrmConnector::GetEdidBlob(drmModePropertyBlobPtr &blob) {
   }
 
   blob = drmModeGetPropertyBlob(drm_->fd(), blob_id);
-  return 0;
+  return !blob;
 }
 
 uint32_t DrmConnector::id() const {